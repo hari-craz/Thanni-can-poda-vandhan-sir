52906fde90f56f3e42449bddfb33790cc454c6d9
hari-craz <harikavi1301@gmail.com>
2026-06-15 19:58:33 +0530

    chore: remove project documentation files and add backend cache directory entries

--- frontend/src/services/anomalies.ts
+++
@@ -1,7 +0,0 @@
-import api from './api'
-import type { AnomaliesListResponse } from '@/types/alert'
-
-export async function fetchAnomalies(): Promise<AnomaliesListResponse> {
-  const { data } = await api.get<AnomaliesListResponse>('/anomalies')
-  return data
-}
--- frontend/src/services/sensorData.ts
+++
@@ -1,9 +0,0 @@
-import api from './api'
-import type { SensorDataResponse } from '@/types/sensor'
-
-export async function fetchSensorData(deviceId: string, limit = 100): Promise<SensorDataResponse> {
-  const { data } = await api.get<SensorDataResponse>(`/data/${deviceId}`, {
-    params: { limit },
-  })
-  return data
-}
